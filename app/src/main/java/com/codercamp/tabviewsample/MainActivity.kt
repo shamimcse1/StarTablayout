8bdb425479ecba84cff29785e6b29c406b2fc498
shamimcse1 <mdshamimh529@gmail.com>
2023-01-15 09:07:54 +0600

    Fixed some issue

--- app/src/main/java/com/codercamp/tabviewsample/MainActivity.kt
+++ app/src/main/java/com/codercamp/tabviewsample/MainActivity.kt
@@ -3,11 +3,14 @@ package com.codercamp.tabviewsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,10 +26,10 @@ class MainActivity : ComponentActivity() {
 
                 val tabs = listOf(
                     StarTabItem(title = "Tab 1") {
-                        Greeting("Tab 1")
+                        TabItemTest(item = "tab1")
                     },
                     StarTabItem(title = "Tab 2") {
-                        Greeting("Tab 2")
+                        TabItemTest(item = "tab2")
                     },
                 )
                 // A surface container using the 'background' color from the theme
@@ -50,7 +53,19 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
-
+@Composable
+fun TabItemTest(item: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = item,
+            color = Color.Black,
+        )
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
